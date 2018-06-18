a2e3c1f3a8ff048b21957c01fdb04010f2f171ce
scope <scope@scopes-MacBook-Pro-2.local>
2018-06-18 11:40:56 -0400

    iOS info

---
+++ iOS_ros_stream/Config.swift
@@ -0,0 +1,17 @@
+//
+//  Config.swift
+//  iOS_ros_stream
+//
+//  Created by scope on 6/12/18.
+//  Copyright © 2018 occamlab. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    
+    struct Ports {
+        static let broadcast = UInt16(35601)
+    }
+    
+}
